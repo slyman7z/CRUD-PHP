3087cfd60cdbfa24509ae6fd94ff82036c999476
suleiman <isuleiman37@gmail.com>
2023-03-31 17:25:54 +0100

    initial commit

---
+++ user.php
@@ -0,0 +1,98 @@
+<?php
+include 'connect.php';
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO `crud` (name, email , mobile, password) VALUES('$name','$email','$mobile','$password') ";
+    $result = mysqli_query($con, $sql);
+    if ($result) {
+        header('location:display.php');
+    } else {
+        die(mysqli_error($con));
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Crud operation</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-secondary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-5" style="max-width: 40rem;">
+        <div class="text-center bg-info-subtle p-4 mb-4 rounded">
+            <h3>Registration Form </h3>
+        </div>
+        <form method="POST" action="" class="shadow p-3">
+            <div class="mb-3">
+                <label for="name" class="form-label text-warning">Username:</label>
+                <input type="text" class="form-control " id="name" placeholder="Enter your username" name="name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label text-warning">Email:</label>
+                <input type="text" class="form-control" id="email" placeholder="Email address........" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="mobile" class="form-label text-warning">Mobile</label>
+                <input type="number" maxlength="11" class="form-control" placeholder="Enter phone number........" id="mobile" name="mobile">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label text-warning">Password:</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter password...........">
+            </div>
+
+            <button type="submit" class="btn btn-outline-primary " name='submit'>Submit</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
